fixed normalization code to work with grayscale images; fixed stokre data person 43; re-render norm. option

normalize.py
@@ -1,69 +1,101 @@
 import numpy as np
-from PIL import Image
+from scipy import misc
 import matplotlib.pyplot as plt
+# from PIL import Image
 
 def normalize(img,figure_size,ground_size):
-	# 
-	#   input : img as boolean numpy array, True means black pixel
+	#   input : img as a float boolean numpy array (bounded between 0 and 1, where 1 is "black")
+	assert_valid_format(img)
 	img = __crop(img)
+	assert_valid_format(img,check_polarity=False)
 	img = __resize(img,maxsize=figure_size)
-	temp = np.array(img)
-	assert max(temp.shape[0],temp.shape[1]) == figure_size
+	assert_valid_format(img,check_polarity=False)
+	assert max(img.shape[0],img.shape[1]) == figure_size
 	img = __paste(img,sz=ground_size)
 	assert np.all(__com(img) == np.array([img.shape[0]/2,img.shape[1]/2]))
 	return img
 
+def assert_valid_format(img,check_polarity=True):
+	# check the format of the image...
+	mx = np.amax(img)
+	mn = np.amin(img)
+	assert float(mx) <= 1.0
+	assert float(mn) >= 0.0
+	assert np.issubdtype(img.dtype, np.float) | np.issubdtype(img.dtype, np.bool_)
+	if check_polarity:
+		on_count = np.sum(img.flatten()>=1e-4)
+		off_count = np.sum(img.flatten()<1e-4)
+		assert off_count > on_count
+
 def __crop(img):
-	# Crop binary image to rectangle that contains minimal amount of white space
-	#   input : img as boolean numpy array, True means black pixel
-	
-
-	on_count = np.sum(img.flatten()>=1e-4)
-	off_count = np.sum(img.flatten()<1e-4)
-	assert off_count > on_count
-
+	# Crop image to rectangle that contains minimal amount of white space
 	xcord,ycord = np.nonzero(img)
 	xmin = np.amin(xcord)
 	ymin = np.amin(ycord)
 	xmax = np.amax(xcord)
 	ymax = np.amax(ycord)
-	I = Image.fromarray(np.uint8(img))
-	return I.crop((ymin,xmin,ymax+1,xmax+1))
+	return img[xmin:xmax+1,ymin:ymax+1]
+	# convert to PIL format
+	# I = Image.fromarray( np.uint8(255.0*(1-img)) )
+	# return I.crop((ymin,xmin,ymax+1,xmax+1))
 
-def __resize(I,maxsize=100):
+def __resize(img,maxsize=100):
 	# Resize an image so that the size of its longest side is 100, maintains aspect ratio
-	#   I : PIL image
-	ratio = maxsize / float(max(I.size[0],I.size[1]))
-	size = [int(round(ratio*I.size[0])), int(round(ratio*I.size[1]))]
-	return I.resize(size, Image.ANTIALIAS)
+	#   img : numpy image
+	ratio = maxsize / float(max(img.shape[0],img.shape[1]))
+	size = [int(round(ratio*img.shape[0])), int(round(ratio*img.shape[1]))]	
+	if np.issubdtype(img.dtype, np.float):
+		out = misc.imresize(img,size,mode='F')
+	elif np.issubdtype(img.dtype, np.bool_):		
+		out = misc.imresize(img.astype(float),size,mode='F')
+		out = out.astype(bool)
+	else:
+		assert False
+	return out
+	# return I.resize(size, Image.ANTIALIAS)
 
-def __com(I):
+def __com(img):
 	# Compute center of mass for all black pixels
-	#   I : binary PIL image
-	xcord,ycord = np.nonzero(I)
-	return np.array([np.mean(xcord),np.mean(ycord)],dtype=int)
+	#   img : numpy image
+	img = img / float(np.sum(img.flatten()))	
+	
+	# sz = img.shape
+	# m = np.array([0.0,0.0])
+	# for x in range(sz[0]):
+	# 	for y in range(sz[1]):
+	# 		m = m + np.array([x,y]) * img[x,y]
+	# m = np.round(m).astype(int)
 
-def __paste(I,sz=250):
-	# Paste a smaller image I (rectangular) into the center of a sz x sx square image
-	#   I : binary PIL image
-	bg = np.zeros((sz,sz),dtype=bool)
+	xcord,ycord = np.nonzero(img)
+	weight = img[xcord,ycord].astype(float)
+	m1 = np.array([np.dot(xcord,weight),np.dot(ycord,weight)])
+	m1 = np.round(m1).astype(int)
+	return m1
+
+	# xcord,ycord = np.nonzero(I)
+	# return np.array([np.mean(xcord),np.mean(ycord)],dtype=int)
+
+def __paste(img,sz=250):
+	# Paste a smaller image img (rectangular) into the center of a sz x sx square image
+	#   img : numpy image
+	bg = np.zeros((sz,sz),dtype=img.dtype)
 	bg_center = np.array([sz/2,sz/2],dtype=int)
-	xcord,ycord = np.nonzero(I)
-	com = __com(I)
+	xcord,ycord = np.nonzero(img)
+	com = __com(img)
 	offset = bg_center - com
-	bg[xcord+offset[0],ycord+offset[1]] = True
+	img = np.asarray(img)
+	bg[xcord+offset[0],ycord+offset[1]] = img[xcord,ycord]
 	return bg
-
 
 if __name__ == "__main__":
 	
 	# Parameters
-	lensize = 77
-	imgsize = 200
+	lensize = 75
+	imgsize = 215
 
 	# Check cropping functionality
 	I = np.zeros((10,10)).astype(bool)
-	I[1,1] = 1
+	I[1,1] = 0.5
 	I[1,2] = 1
 	I[1,3] = 1
 	I[1,4] = 1
@@ -71,22 +103,25 @@ if __name__ == "__main__":
 	I[3,1] = 1
 	I[4,1] = 1
 	I[5,1] = 1
-	I[6,1] = 1
+	I[6,1] = 0.5
+	
 	I2 = __crop(I)
-	# assert np.all(I[1:7,1:5] == I2)
+	assert np.all(I[1:7,1:5] == I2)
+	
 	print "Cropping test passed..."
 
 	I3 = __resize(I2,maxsize=lensize)
 	I4 = __paste(I3,sz=imgsize)
-
 	I3 = np.array(I3)
 	I4 = np.array(I4)
 
+	# assert I4.dtype == I.dtype
 	assert max(I3.shape[0],I3.shape[1]) == lensize
 	print "Resize test passed..."
 	assert np.all(__com(I4) == np.array([I4.shape[0]/2,I4.shape[1]/2]))
 	print "Pasting test passed..."
 
+	I4 = normalize(I,lensize,imgsize)
 	plt.figure(1)
-	plt.imshow(I4)
+	plt.imshow(1-I4,cmap='Greys_r')
 	plt.show()

objects/shadow.py
@@ -8,10 +8,13 @@ import os
 from skimage import morphology, measure
 import cv2
 import sys
+sys.path.append("../")
+from normalize import normalize
+
 
 # Parameters
 if len(sys.argv) <= 1:
-	dir_in = 'horse' # directory with resized images (approx. 500 pixels in longest dimension)
+	dir_in = 'cat' # directory with resized images (approx. 500 pixels in longest dimension)
 else:
 	dir_in = sys.argv[1]
 threshold = 10 # the modal value subtracted by this number is used to separate figure from ground (assuming white background)
@@ -19,6 +22,8 @@ img_size = 7 # size of contour plot in inches (shouldn't matter)
 sub_scale = 0.5 # reduce size of primary object by this much
 outsize = 250 # size of the output image
 rename = True # rename the files?
+do_normalize = True # should we normalize the images?
+figure_size = 125 # size of the largest dimension of the image (keeping aspect ratio)
 
 def load_images(mydir):
 	list_fn = os.listdir(mydir)
@@ -55,12 +60,16 @@ def save_gscale_img(fn,grayimg,mask_erode,mask_dilate):
 	img = grayimg 
 	img = np.maximum(img,mask_erode)
 	img = np.minimum(img,mask_dilate)
-
-	out = 1-img
-	out = out * 255
-	out = out.astype(int)
-
-	out = misc.imresize(out,(outsize,outsize))
+	if do_normalize:
+		img = normalize(img,figure_size,outsize)		
+		out = 1-img
+		out = out * 255
+		out = out.astype(int)
+	else:
+		out = 1-img
+		out = out * 255
+		out = out.astype(int)
+		out = misc.imresize(out,(outsize,outsize))
 	misc.imsave(fn,out)
 
 # compute the size of the largest floating background object in an image (island of white or black pixels)

render.py
@@ -2,6 +2,10 @@ import csv
 import Tkinter as tk
 import copy
 import numpy as np
+import os
+import glob
+import shutil
+from numpy.matlib import repmat
 
 #
 # Return pen sequence from csv file as image.
@@ -25,7 +29,39 @@ import numpy as np
 #
 sz = 250 # size of canvas
 lw = 8.0 # line width
-rd = lw/2.0 # radius of dot
+rd = lw/2.025
+# rd = lw/2.0 # radius of dot
+
+do_normalize = True # do we want to normalize images?
+figure_size = 125 # size of the largest dimension of the image (keeping aspect ratio)
+
+def __resize(strokes,maxsize):
+	# normalize character so that longest side is maxsize long
+	v = flatten(strokes)
+	if v.shape[0]==1: # if we only have length one
+		return strokes
+	vx = v[:,0]
+	vy = v[:,1]	
+	rx = np.amax(vx)-np.amin(vx)
+	ry = np.amax(vy)-np.amin(vy)
+	ratio = maxsize / float(max(rx,ry))
+	new_strokes = np.copy(strokes)
+	new_strokes = [s*ratio for s in new_strokes]
+	assert np.isclose(np.amax(np.amax(v*ratio,axis=0)-np.amin(v*ratio,axis=0)),maxsize)
+	return new_strokes
+
+def __center(strokes):
+	# center image so that center of mass (in trajectory space) is at center of the image
+	v = flatten(strokes)
+	m = np.mean(v,axis=0)
+	bg_center = np.array([sz/2,sz/2],dtype=float)
+	offset = bg_center - m
+	new_strokes = []
+	for s in strokes:
+		N = s.shape[0]
+		new_strokes.append(s + repmat(offset,N,1))
+	assert np.all(np.isclose(np.mean(flatten(new_strokes),axis=0),bg_center))
+	return new_strokes
 
 def transform(strokes):
 	for stk in strokes:
@@ -37,6 +73,8 @@ def transform(strokes):
 	return strokes
 
 def flatten(strokes):
+	if strokes==[]:
+		return np.array([])
 	overall = strokes[0]
 	for stk in strokes[1:]:
 		overall = np.append(overall,stk,axis=0)
@@ -49,7 +87,7 @@ def load_traj(fn):
 	with open(fn,'rb') as f:
 		reader = csv.reader(f)
 		for line in reader:
-			stk = [item[1:-1].split(', ') for item in line]
+			stk = [item[1:-1].split(', ') for item in line]			
 			assert len(stk[0])==3 # make sure format is [x,y,t]
 			strokes.append(np.array(stk,dtype='float'))
 
@@ -62,12 +100,16 @@ def load_traj(fn):
 			dtime.append(new_time-time)
 			time = new_time
 	dtime = np.array(dtime)
-	print "average time resolution: " + str(int(np.mean(dtime))) + " ms"
-	print "max time resolution: " + str(int(np.max(dtime))) + " ms"
-	print "min time resolution: " + str(int(np.min(dtime))) + " ms"
+	
+	# print "average time resolution: " + str(int(np.mean(dtime))) + " ms"
+	# print "max time resolution: " + str(int(np.max(dtime))) + " ms"
+	# print "min time resolution: " + str(int(np.min(dtime))) + " ms"
 
 	strokes = [stk[:,0:2] for stk in strokes]
 	strokes = transform(strokes)
+	if do_normalize and len(strokes)>0:
+		strokes = __resize(strokes,figure_size)
+		strokes = __center(strokes)
 	return strokes
 
 def render(strokes):
@@ -77,21 +119,38 @@ def render(strokes):
 	w.pack()
 	for stk in strokes:
 		pos = stk[0][0:2]
-		w.create_oval(pos[0]-rd, pos[1]-rd, pos[0]+rd, pos[1]+rd, fill="black")
+		w.create_oval(pos[0]-rd, pos[1]-rd, pos[0]+rd, pos[1]+rd, fill="black", width=0)
 		for idx in range(1,len(stk)):			
 			new_pos = stk[idx][0:2]
 			w.create_line(pos[0], pos[1], new_pos[0], new_pos[1], fill="black", width=lw)
-			w.create_oval(new_pos[0]-rd, new_pos[1]-rd, new_pos[0]+rd, new_pos[1]+rd, fill="black")
+			w.create_oval(new_pos[0]-rd, new_pos[1]-rd, new_pos[0]+rd, new_pos[1]+rd, fill="black", width=0)
 			pos = copy.copy(new_pos)			
 
 	filename = 'render'
 	w.postscript(file=filename+'.ps',width=sz,height=sz)
 	w.delete(tk.ALL)
+	os.system('convert -flatten ' + filename+'.ps ' + filename+'.png')
+	os.remove(filename+'.ps')
+	return filename+'.png'
 
 if __name__ == "__main__":
-	fn = 'strokes_handwritten/s43_aaa_practice_01.csv'
-	strokes = load_traj(fn)
-	render(strokes)
+	# fn = 'strokes_handwritten/s1_aaa_practice_01.csv'
+	# strokes = load_traj(fn)
+	# render(strokes)
+
+	# re-render all of the iamges
+	indir = 'strokes_handwritten'
+	outdir = 'imgs_rerender'
+	if not os.path.exists(outdir): os.makedirs(outdir)
+	files = glob.glob(indir+'/*.csv')
+	for idx,f in enumerate(files):
+		new_name = outdir + '/' + f[len(indir)+1:]
+		new_name = new_name.replace('.csv','.png')
+		if not os.path.isfile(new_name):
+			print "rendering " + str(idx) + " of " + str(len(files))
+			tempfile = render(load_traj(f))			
+			shutil.copy(tempfile,new_name)
+			os.remove(tempfile)
 
 	## Check that the transform worked
 	# ---

select_stimuli.py
@@ -19,7 +19,8 @@ background_size = 250 # total image size
 imgs_by_type = 'selected' # directory to store aggregated images
 exclude = 'exclude.txt' # particular images we want to exclude
 imgs_printed = 'imgs_printed' # images we want participant to copy
-imgs_handwritten = 'imgs_handwritten' # folder to store drawn images
+imgs_handwritten = 'imgs_rerender' # folder to store drawn images
+# imgs_handwritten = 'imgs_handwritten' # folder to store drawn images
 nstim_per_category = 40 # number of examples we want for each category
 
 if not os.path.exists(imgs_by_type): os.makedirs(imgs_by_type)
@@ -92,12 +93,12 @@ for b in bases:
 		# normalize stimuli
 		srcfile = dstdir + '/s' + str(s) + '_' + b + '.png'
 		img = misc.imread(srcfile, flatten=True)
-		img = np.array(img)
+		img = np.array(img,dtype=float)
 		mx = np.amax(img.flatten())
-		img = np.array(img < mx/2) # convert to boolean image
+		img = img / float(mx)
+		img = 1-img
 		img = normalize(img,figure_size,background_size)
-		img = np.logical_not(img)
-		misc.imsave(srcfile,img)
+		misc.imsave(srcfile,1-img)
 
 		# rename file
 		dstfile = dstdir + '/item' + tostr(count) + '_' + b + '.png'					